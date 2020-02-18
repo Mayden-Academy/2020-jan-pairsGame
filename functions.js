a1a57f6aaa2f2feb4a7c88276cdb3c9f0c2b74cc
mathewjmartin <matthewjmartin@zoho.com>
2020-02-18 14:48:50 +0000

    created cardShow function

--- functions.js
+++ functions.js
@@ -1,9 +1,19 @@
 function assignCards (randomizedArray) {
     let cards = document.querySelectorAll('.card')
-    console.log (cards)
     let i = 0
     cards.forEach(function(card) {
         card.dataset.id = randomizedArray[i].id
         i++
     })
+}
+
+function cardShow (event, randomizedCards) {
+    let targetUrl = ''
+    let dataId = event.target.dataset.id
+    randomizedCards.forEach(function (card) {
+        if (dataId == card.id) {
+            targetUrl = card.url
+        }
+    })
+    event.target.innerHTML = `<img src="${targetUrl}"/>`
 }
