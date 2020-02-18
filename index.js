049f6bf3a274e62d49336f0192c58f7da6808444
ajhswan <andrewswan.dev@gmail.com>
2020-02-18 14:31:10 +0000

    called randomiseDeck function in index.js file

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ fetch('./deck.json')
     .then(data => data.json())
     .then((fetchDeck)=>{
         let deck = fetchDeck.hippos
+        deck = randomiseDeck(deck)
         assignCards(deck)
     })
+
